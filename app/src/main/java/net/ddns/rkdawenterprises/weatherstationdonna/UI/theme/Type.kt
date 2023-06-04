2509c384e54d2a71bfdab336c5e260c60b8ed893
Ralph Williamson <rkdawenterprises@gmail.com>
2023-06-04 11:12:36 -0500

    Composing the UI.

--- app/src/main/java/net/ddns/rkdawenterprises/weatherstationdonna/UI/theme/Type.kt
+++ app/src/main/java/net/ddns/rkdawenterprises/weatherstationdonna/UI/theme/Type.kt
@@ -12,6 +12,7 @@ import androidx.compose.material.Typography
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
 import net.ddns.rkdawenterprises.weatherstationdonna.R
@@ -19,73 +20,80 @@ import net.ddns.rkdawenterprises.weatherstationdonna.R
 /**
  * https://fonts.google.com/specimen/Montserrat
  */
-private val Montserrat = FontFamily(
-    Font(R.font.montserrat_regular),
+private val Montserrat = FontFamily(Font(R.font.montserrat_regular),
     Font(R.font.montserrat_medium, FontWeight.W500),
-    Font(R.font.montserrat_semibold, FontWeight.W600)
-)
+    Font(R.font.montserrat_semibold, FontWeight.W600))
 
 /**
  * https://fonts.google.com/specimen/Domine
  */
-private val Domine = FontFamily(
-    Font(R.font.domine_regular),
-    Font(R.font.domine_bold, FontWeight.Bold)
-)
+private val Domine = FontFamily(Font(R.font.domine_regular),
+    Font(R.font.domine_bold, FontWeight.Bold));
+
+/**
+ * https://fonts.google.com/specimen/Roboto+Condensed
+ */
+private val roboto_condensed = FontFamily(Font(R.font.roboto_condensed_regular),
+                                         Font(R.font.roboto_condensed_bold, FontWeight.Bold),
+                                         Font(R.font.roboto_condensed_light, FontWeight.Light),
+                                         Font(resId = R.font.roboto_condensed_italic,
+                                              weight = FontWeight.Normal,
+                                              style = FontStyle.Italic));
 
 val Main_typography = Typography(
     h3 = TextStyle(
-        fontFamily = Montserrat,
-        fontWeight = FontWeight.W600,
+        fontFamily = roboto_condensed,
+        fontWeight = FontWeight.Bold,
         fontSize = 36.sp
     ),
     h4 = TextStyle(
-        fontFamily = Montserrat,
-        fontWeight = FontWeight.W600,
+        fontFamily = roboto_condensed,
+        fontWeight = FontWeight.Bold,
         fontSize = 30.sp
     ),
     h5 = TextStyle(
-        fontFamily = Montserrat,
-        fontWeight = FontWeight.W600,
+        fontFamily = roboto_condensed,
+        fontWeight = FontWeight.Bold,
         fontSize = 24.sp
     ),
     h6 = TextStyle(
-        fontFamily = Montserrat,
-        fontWeight = FontWeight.W600,
+        fontFamily = roboto_condensed,
+        fontWeight = FontWeight.Bold,
         fontSize = 20.sp
     ),
     subtitle1 = TextStyle(
-        fontFamily = Montserrat,
-        fontWeight = FontWeight.W600,
+        fontFamily = roboto_condensed,
+        fontWeight = FontWeight.Bold,
         fontSize = 16.sp
     ),
     subtitle2 = TextStyle(
-        fontFamily = Montserrat,
-        fontWeight = FontWeight.W500,
+        fontFamily = roboto_condensed,
+        fontWeight = FontWeight.Normal,
         fontSize = 14.sp
     ),
     body1 = TextStyle(
-        fontFamily = Domine,
+        fontFamily = roboto_condensed,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp
     ),
     body2 = TextStyle(
-        fontFamily = Montserrat,
+        fontFamily = roboto_condensed,
+        fontWeight = FontWeight.Normal,
         fontSize = 14.sp
     ),
     button = TextStyle(
-        fontFamily = Montserrat,
-        fontWeight = FontWeight.W500,
+        fontFamily = roboto_condensed,
+        fontWeight = FontWeight.Normal,
         fontSize = 14.sp
     ),
     caption = TextStyle(
-        fontFamily = Montserrat,
+        fontFamily = roboto_condensed,
         fontWeight = FontWeight.Normal,
         fontSize = 12.sp
     ),
     overline = TextStyle(
-        fontFamily = Montserrat,
-        fontWeight = FontWeight.W500,
+        fontFamily = roboto_condensed,
+        fontWeight = FontWeight.Normal,
         fontSize = 12.sp
     )
 )
